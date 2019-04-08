b4115cd51403281753f36fe56cdf80027ef06aec
wangyang <869123771@qq.com>
2019-04-08 14:31:38 +0800

    reparer login

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -5,15 +5,34 @@
                 <Col span="5">
                     <Collapse v-model="collapse">
                         <CollapseItem title="login" value="loginItem">
-                            <Form @submit="submit" ref="form" labelWidth = "0">
-                                <FormItem model="form.username" :rules="{required: true}">
-                                    <Input v-model="form.username" clearable/>
+                            <Form layout="vertical" ref="form" class = "mt-3">
+                                <FormItem model="form.username"
+                                          :rules = "formRule.username.rules"
+                                          :messages = "formRule.username.messages"
+                                          hideLabel>
+                                    <Input v-model="form.username" size = "large" clearable placeholder = "帐号">
+                                        <template slot="prefix"><i class="ion-person"></i></template>
+                                    </Input>
                                 </FormItem>
-                                <FormItem model="model.password" :rules="{required: true}">
-                                    <Input type="password" v-model="form.password" clearable/>
+                                <FormItem model="form.password"
+                                          :rules = "formRule.password.rules"
+                                          :messages = "formRule.password.messages"
+                                          hideLabel>
+                                    <Input type="password" v-model="form.password" size = "large" clearable placeholder = "密码">
+                                        <template slot="prefix"><i class="ion-locked"></i></template>
+                                    </Input>
                                 </FormItem>
-                                <FormItem class="form-item-button mb-4">
-                                    <Button type="primary" fluid>登录</Button>
+                                <FormItem model="form.way"
+                                          :rules = "formRule.way.rules"
+                                          :messages = "formRule.way.messages"
+                                          hideLabel>
+                                    <Select v-model="form.way" clearable fluid placeholder = "登录方式">
+                                        <Option value="local">本地登录</Option>
+                                        <Option value="uniportal">w3登录</Option>
+                                    </Select>
+                                </FormItem>
+                                <FormItem class="form-item-button mb-4" hideLabel>
+                                    <Button type="primary" size = "large" fluid @click = "handleSubmit">登录</Button>
                                 </FormItem>
                             </Form>
                         </CollapseItem>
@@ -26,7 +45,7 @@
 </template>
 
 <script>
-    import {Row, Col, Button, Form, FormItem, Input, Collapse, CollapseItem} from 'kpc'
+    import {Row, Col, Button, Form, FormItem, Input,Select,Option,Collapse, CollapseItem} from 'kpc'
     import vueParticleLine from 'vue-particle-line'
     import 'vue-particle-line/dist/vue-particle-line.css'
     import Vue from 'vue'
@@ -35,16 +54,42 @@
     export default {
         name: "index",
         components: {
-            Row, Col, Button, Form, FormItem, Input, Collapse, CollapseItem
+            Row, Col, Button, Form, FormItem, Input,Select,Option,Collapse, CollapseItem
         },
         data() {
+            const rules = {
+                rules : {required: true},
+                messages : {required:'必填'}
+            }
             return {
                 collapse: ['loginItem'],
                 form: {
                     username: '',
-                    password: ''
+                    password: '',
+                    way : ''
+                },
+                formRule : {
+                    username : {
+                        ...rules
+                    },
+                    password : {
+                        ...rules
+                    },
+                    way : {
+                        ...rules
+                    }
                 }
             }
+        },
+        methods : {
+            async handleSubmit() {
+                let valid = this.$refs.form.validate()
+                if (valid) {
+
+                } else {
+
+                }
+            },
         }
     }
 </script>
@@ -55,17 +100,12 @@
         .z-1000 {
             z-index: 1000;
         }
-        .form-item-button .k-content {
-            width: 100%;
-        }
         .k-collapse{
             background-color: #f8f9fa;
         }
         /deep/ .slot-wraper {
             height: 100%;
         }
-        /deep/ .k-collapse-item > .k-content > .k-wrapper {
-            padding: 0px;
-        }
+
     }
 </style>
